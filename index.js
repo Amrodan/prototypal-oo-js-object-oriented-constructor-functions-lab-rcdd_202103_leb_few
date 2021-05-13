e1d0a5440a79b5619e0121baed2e4c7d0e51386a
Amro dandashli <amrdandashli@gmail.com>
2021-05-13 06:51:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function Scooter(year, color,model) {
+    this.name = name;
+    this.email = email;
+    this.model = model 
+   
+}
+
+
+function Driver (name, age,experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+         
+   
+}
+
+
+function PickupLocation  (address,city) {
+    this.address = address;
+    this.city = city;
+//     this.locattion = function() {
+//         console.log(`Hello everybody, my name is ${this.name} whom you've been
+// mailing at ${this.email}!`);
+//     };
+}
